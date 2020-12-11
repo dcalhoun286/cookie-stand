6dccf2021fdea23191bfecdc2bfb64e7308d2ab3
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2020-12-10 18:24:27 -0800

    added form to sales.html and confirmed functionality of user input on form

--- js/app.js
+++ js/app.js
@@ -20,7 +20,36 @@ var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '
 var stores = [];
 var theadElement = document.createElement('thead');
 var trElement = document.createElement('tr');
+
+// global variables declared for user input on form
+var citystorename;
+var minimum;
+var maximum;
+var average;
 // var thElement = document.createElement('th');
+
+// step 1: get element by id to listen to container
+var myForm = document.getElementById('form');
+
+//step 3: event handler
+function handleSubmit(event) {
+  event.preventDefault();
+
+  citystorename = event.target.citystorename.value;
+  console.log(citystorename);
+
+  minimum = event.target.minimum.value;
+  console.log(minimum);
+
+  maximum = event.target.maximum.value;
+  console.log(maximum);
+
+  average = event.target.average.value;
+  console.log(average);
+}
+
+
+
 
 storeTable.appendChild(theadElement);
 
@@ -59,7 +88,7 @@ function Store (name, min, max, avg) {
     trElement.appendChild(thElement);
     // var tdElement = document.createElement('td');
     // for (var i = 0; i < hours.length; i++) {
-    //   tdElement.textContent = this.calculateAndPopulateHourlySales();
+      //   tdElement.textContent = this.calculateAndPopulateHourlySales();
     //   trElement.appendChild(tdElement);
     // }
     this.calculateAndPopulateHourlySales();
@@ -71,7 +100,7 @@ function Store (name, min, max, avg) {
       // append to DOM
       trElement.appendChild(tdElement);
     }
-
+    
     var lasttdElement = document.createElement('td');
     lasttdElement.textContent = `Total: ${this.dailyTotal} cookies`;
     trElement.appendChild(lasttdElement);
@@ -85,7 +114,7 @@ function Store (name, min, max, avg) {
     //   console.log(random);
     //   return random;
   };
-
+  
   this.calculateAndPopulateHourlySales = function(){
     for ( var i = 0; i < hours.length; i++){
       var hourlyTotal = Math.ceil(this.getRandomCustomersPerHour() * this.avg)
@@ -108,217 +137,19 @@ var tokyo = new Store('Tokyo', 3, 24, 1.2);
 var dubai = new Store('Dubai', 11, 38, 3.7);
 var paris = new Store('Paris', 20, 38, 2.3);
 var lima = new Store('Lima', 2, 16, 4.6);
+// var userstore = new Store(citystorename, parseInt(minimum), parseInt(maximum), parseFloat(average));
+// console.log(userstore);
+
 // functions / prototype methods
 Store.prototype.storeInfo = function() {
 
 };
+
 seattle.render();
 tokyo.render();
 dubai.render();
 paris.render();
 lima.render();
 
-// var seattle = {
-//   name: 'Seattle',
-//   min: 23,
-//   max: 65,
-//   avg: 6.3,
-//   hourlySales: [],
-//   dailyTotal: 0,
-
-
-
-//   // iteratively render each list item
-//   render: function() {
-//   }
-// };
-
-// var tokyo = {
-//   name: 'Tokyo',
-//   min: 3,
-//   max: 24,
-//   avg: 1.2,
-//   hourlySales: [],
-//   dailyTotal: 0,
-
-
-//   // successfully get random number between min and max provided
-//   getRandomCustomersPerHour: function(){
-//     return Math.floor(Math.random()*(this.max - this.min + 1) + this.min);
-//   //  var random = Math.floor(Math.random()*(this.max - this.min + 1) + this.min);
-//   //   console.log(random);
-//   //   return random;
-//   },
-
-//   calculateAndPopulateHourlySales: function(){
-//     for ( var i = 0; i < hours.length; i++){
-//       var hourlyTotal = Math.ceil(this.getRandomCustomersPerHour() * this.avg)
-//       ;
-//       console.log(hourlyTotal);
-//       this.hourlySales[i] = hourlyTotal;
-//       this.dailyTotal += hourlyTotal;
-//     }
-//   },
-
-//   // iteratively render each list item
-//   render: function() {
-//     this.calculateAndPopulateHourlySales();
-//     for (var i = 0; i < hours.length; i++) {
-//       var liElement = document.createElement('li');
-//       // the text inside the li element I just created
-//       liElement.textContent = `${hours[i]}: ${this.hourlySales[i]} cookies`;
-//       // append to DOM
-//       tokyoList.appendChild(liElement);
-//     }
-
-//     liElement = document.createElement('li');
-//     liElement.textContent = `Total: ${this.dailyTotal} cookies`;
-//     tokyoList.appendChild(liElement);
-//   }
-// };
-
-// var dubai = {
-//   name: 'Dubai',
-//   min: 11,
-//   max: 38,
-//   avg: 3.7,
-//   hourlySales: [],
-//   dailyTotal: 0,
-
-
-//   // successfully get random number between min and max provided
-//   getRandomCustomersPerHour: function(){
-//     return Math.floor(Math.random()*(this.max - this.min + 1) + this.min);
-//   //  var random = Math.floor(Math.random()*(this.max - this.min + 1) + this.min);
-//   //   console.log(random);
-//   //   return random;
-//   },
-
-//   calculateAndPopulateHourlySales: function(){
-//     for ( var i = 0; i < hours.length; i++){
-//       var hourlyTotal = Math.ceil(this.getRandomCustomersPerHour() * this.avg)
-//       ;
-//       console.log(hourlyTotal);
-//       this.hourlySales[i] = hourlyTotal;
-//       this.dailyTotal += hourlyTotal;
-//     }
-//   },
-
-//   // iteratively render each list item
-//   render: function() {
-//     this.calculateAndPopulateHourlySales();
-//     for (var i = 0; i < hours.length; i++) {
-//       var liElement = document.createElement('li');
-//       // the text inside the li element I just created
-//       liElement.textContent = `${hours[i]}: ${this.hourlySales[i]} cookies`;
-//       // append to DOM
-//       dubaiList.appendChild(liElement);
-//     }
-
-//     liElement = document.createElement('li');
-//     liElement.textContent = `Total: ${this.dailyTotal} cookies`;
-//     dubaiList.appendChild(liElement);
-//   }
-// };
-
-// var paris = {
-//   name: 'Paris',
-//   min: 20,
-//   max: 38,
-//   avg: 2.3,
-//   hourlySales: [],
-//   dailyTotal: 0,
-
-
-//   // successfully get random number between min and max provided
-//   getRandomCustomersPerHour: function(){
-//     return Math.floor(Math.random()*(this.max - this.min + 1) + this.min);
-//   //  var random = Math.floor(Math.random()*(this.max - this.min + 1) + this.min);
-//   //   console.log(random);
-//   //   return random;
-//   },
-
-//   calculateAndPopulateHourlySales: function(){
-//     for ( var i = 0; i < hours.length; i++){
-//       var hourlyTotal = Math.ceil(this.getRandomCustomersPerHour() * this.avg)
-//       ;
-//       console.log(hourlyTotal);
-//       this.hourlySales[i] = hourlyTotal;
-//       this.dailyTotal += hourlyTotal;
-//     }
-//   },
-
-//   // iteratively render each list item
-//   render: function() {
-//     this.calculateAndPopulateHourlySales();
-//     for (var i = 0; i < hours.length; i++) {
-//       var liElement = document.createElement('li');
-//       // the text inside the li element I just created
-//       liElement.textContent = `${hours[i]}: ${this.hourlySales[i]} cookies`;
-//       // append to DOM
-//       parisList.appendChild(liElement);
-//     }
-
-//     liElement = document.createElement('li');
-//     liElement.textContent = `Total: ${this.dailyTotal} cookies`;
-//     parisList.appendChild(liElement);
-//   }
-// };
-
-// var lima = {
-//   name: 'Lima',
-//   min: 2,
-//   max: 16,
-//   avg: 4.6,
-//   hourlySales: [],
-//   dailyTotal: 0,
-
-
-//   // successfully get random number between min and max provided
-//   getRandomCustomersPerHour: function(){
-//     return Math.floor(Math.random()*(this.max - this.min + 1) + this.min);
-//   //  var random = Math.floor(Math.random()*(this.max - this.min + 1) + this.min);
-//   //   console.log(random);
-//   //   return random;
-//   },
-
-//   calculateAndPopulateHourlySales: function(){
-//     for ( var i = 0; i < hours.length; i++){
-//       var hourlyTotal = Math.ceil(this.getRandomCustomersPerHour() * this.avg)
-//       ;
-//       console.log(hourlyTotal);
-//       this.hourlySales[i] = hourlyTotal;
-//       this.dailyTotal += hourlyTotal;
-//     }
-//   },
-
-//   // iteratively render each list item
-//   render: function() {
-//     this.calculateAndPopulateHourlySales();
-//     for (var i = 0; i < hours.length; i++) {
-//       var liElement = document.createElement('li');
-//       // the text inside the li element I just created
-//       liElement.textContent = `${hours[i]}: ${this.hourlySales[i]} cookies`;
-//       // append to DOM
-//       limaList.appendChild(liElement);
-//     }
-
-//     liElement = document.createElement('li');
-//     liElement.textContent = `Total: ${this.dailyTotal} cookies`;
-//     limaList.appendChild(liElement);
-//   }
-// };
-
-
-// executables
-// seattle.render();
-// tokyo.render();
-// dubai.render();
-// paris.render();
-// lima.render();
-
-
-
-
-
-
+// step 2: add event listener - we pass in two arguments! Event as string, and callback function
+myForm.addEventListener('submit', handleSubmit);
