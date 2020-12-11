91b990a2f56169a4b6caecd1db3bff10106f4101
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2020-12-10 23:36:39 -0800

    deleted corpse code and repeat code

--- js/app.js
+++ js/app.js
@@ -1,32 +1,23 @@
 'use strict';
 
-var salmon = {
-  title: 'salmon, adult',
-  src: 'img/salmon.png',
-  alt: 'photo of salmon',
-  colors: [],
-};
+// var salmon = {
+//   title: 'salmon, adult',
+//   src: 'img/salmon.png',
+//   alt: 'photo of salmon',
+//   colors: [],
+// };
 
 // elements from DOM -- lists
 var storeTable = document.getElementById('salestable');
-var seattleList = document.getElementById('seattle');
-var tokyoList = document.getElementById('tokyo');
-var dubaiList = document.getElementById('dubai');
-var parisList = document.getElementById('paris');
-var limaList = document.getElementById('lima');
 
 //global variables
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 var stores = [];
 var theadElement = document.createElement('thead');
-var trElement = document.createElement('tr');
-
-// global variables declared for user input on form
 var citystorename;
 var minimum;
 var maximum;
 var average;
-// var thElement = document.createElement('th');
 
 // step 1: get element by id to listen to container
 var myForm = document.getElementById('form');
@@ -48,9 +39,6 @@ function handleSubmit(event) {
   console.log(average);
 }
 
-
-
-
 storeTable.appendChild(theadElement);
 
 var renderHours = function() {
@@ -63,7 +51,7 @@ var renderHours = function() {
 
   for (var i = 0; i <= hours.length; i++) {
     // create element
-    var thElement = document.createElement('th');
+    thElement = document.createElement('th');
     // give it content
     thElement.textContent = hours[i];
     //append it to the DOM
@@ -80,6 +68,7 @@ function Store (name, min, max, avg) {
   this.max = max;
   this.avg = avg;
   this.hourlySales = [];
+  stores.push(this);
   this.dailyTotal = 0;
   this.render = function() {
     var trElement = document.createElement('tr');
@@ -88,7 +77,7 @@ function Store (name, min, max, avg) {
     trElement.appendChild(thElement);
     // var tdElement = document.createElement('td');
     // for (var i = 0; i < hours.length; i++) {
-      //   tdElement.textContent = this.calculateAndPopulateHourlySales();
+    //   tdElement.textContent = this.calculateAndPopulateHourlySales();
     //   trElement.appendChild(tdElement);
     // }
     this.calculateAndPopulateHourlySales();
@@ -100,32 +89,26 @@ function Store (name, min, max, avg) {
       // append to DOM
       trElement.appendChild(tdElement);
     }
-    
+
     var lasttdElement = document.createElement('td');
-    lasttdElement.textContent = `Total: ${this.dailyTotal} cookies`;
+    lasttdElement.textContent = `${this.dailyTotal}`;
     trElement.appendChild(lasttdElement);
     storeTable.appendChild(trElement);
-    console.log('test');
   };
   // successfully get random number between min and max provided
   this.getRandomCustomersPerHour = function(){
     return Math.floor(Math.random()*(this.max - this.min + 1) + this.min);
-    //  var random = Math.floor(Math.random()*(this.max - this.min + 1) + this.min);
-    //   console.log(random);
-    //   return random;
   };
-  
+
   this.calculateAndPopulateHourlySales = function(){
     for ( var i = 0; i < hours.length; i++){
       var hourlyTotal = Math.ceil(this.getRandomCustomersPerHour() * this.avg)
         ;
-      console.log(hourlyTotal);
       this.hourlySales[i] = hourlyTotal;
       this.dailyTotal += hourlyTotal;
     }
   };
 
-  stores.push(this);
   for (var i = 0; i < stores.length; i++) {
     stores[i];
   }
@@ -137,19 +120,14 @@ var tokyo = new Store('Tokyo', 3, 24, 1.2);
 var dubai = new Store('Dubai', 11, 38, 3.7);
 var paris = new Store('Paris', 20, 38, 2.3);
 var lima = new Store('Lima', 2, 16, 4.6);
-// var userstore = new Store(citystorename, parseInt(minimum), parseInt(maximum), parseFloat(average));
-// console.log(userstore);
-
-// functions / prototype methods
-Store.prototype.storeInfo = function() {
-
-};
+var userstore = new Store(citystorename, minimum, handleSubmit.maximum, handleSubmit.average);
 
 seattle.render();
 tokyo.render();
 dubai.render();
 paris.render();
 lima.render();
+userstore.render();
 
 // step 2: add event listener - we pass in two arguments! Event as string, and callback function
 myForm.addEventListener('submit', handleSubmit);
